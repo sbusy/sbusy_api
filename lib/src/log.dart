c97b200843365bd82085193997c7d69b6404e7d0
Vinogradov Alexander <astronom_35@mail.ru>
2020-11-26 11:05:15 +0300

    Добавлена json сериализация

--- lib/src/log.dart
+++ lib/src/log.dart
@@ -1,8 +1,9 @@
 import 'package:intl/intl.dart';
 
-final DateFormat formatter = DateFormat('yyyy-MM-dd hh:mm:ss');
+final DateFormat _formatter = DateFormat('yyyy-MM-dd hh:mm:ss');
 
+/// Печатает сообщение в формате `[<дата> <время>] : <сообщение>`
 void log(Object? object) {
-  String formatted = formatter.format(DateTime.now());
+  String formatted = _formatter.format(DateTime.now());
   print('[$formatted] : $object');
 }
